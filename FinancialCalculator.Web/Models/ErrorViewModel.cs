ca529c93ac7799e318a9e4481dc5c0d3c76633b3
Denis Vasilev <vassdeniss@gmail.com>
2024-11-05 10:20:09 +0200

    Initial commit

---
+++ FinancialCalculator.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FinancialCalculator.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
