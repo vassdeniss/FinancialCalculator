457b7fb9dd45ee6cfd44fd63868b962603b1ef7f
Denis Vasilev <vassdeniss@gmail.com>
2024-11-05 21:53:01 +0200

    feat: added unit test dummy project

---
+++ FinancialCalculator.Services.UnitTests/LeasingCalculatorUnitTests.cs
@@ -0,0 +1,11 @@
+namespace FinancialCalculator.Services.UnitTests;
+
+public class LeasingCalculatorUnitTests
+{
+    // TODO: Write test cases
+    // Leasing calc link to base tests on: http://www.moitepari.bg/kalkulatori/Lizingov_kalkulator.aspx
+    // Note: Please use AAA comments when writing tests
+    // Note 2: When committing use the template of "<action>: <comment>"
+    // Actions can be of "feat" (feature), "refactor", "fix" or your own that seems fitting
+    // Example of a commit: "feat: wrote unit tests for..."
+}
