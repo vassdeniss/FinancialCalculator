1b885082cec3b39e55e48a573bfd5b31a88d5ac6
tatyana.pencheva.16@gmail.com <Ob1593572486//*//>
2024-11-15 17:25:55 +0200

    feat: implemented fee calculation methods and unit tests
    - Created ICreditService interface for credit operations.
    - Implemented CalculateInitialFees and ConvertPercentageToCurrency methods in CreditService.
    - Added placeholders for CalculateAnnualManagementFee and CalculateOtherAnnualFees.
    - Defined FeeType and PaymentType enums.
    - Added unit tests in CreditServiceUnitTestes to validate fee calculations and edge cases.

--- FinancialCalculator.Services.UnitTests/LeasingCalculatorUnitTests.cs
+++ FinancialCalculator.Services.UnitTests/LeasingCalculatorUnitTests.cs
@@ -8,4 +8,7 @@ public class LeasingCalculatorUnitTests
     // Note 2: When committing use the template of "<action>: <comment>"
     // Actions can be of "feat" (feature), "refactor", "fix" or your own that seems fitting
     // Example of a commit: "feat: wrote unit tests for..."
+
 }
+
+
