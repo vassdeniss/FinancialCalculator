e0cbcaa71515e5dbde8899cedc91e0c84b2cd1c2
Denis Vasilev <vassdeniss@gmail.com>
2024-12-11 17:35:43 +0200

    refactor: UI refactors

---
+++ FinancialCalculator.Web/Controllers/CreditController.cs
@@ -0,0 +1,43 @@
+﻿using FinancialCalculator.Services.Contracts;
+using FinancialCalculator.Services.DTO;
+using FinancialCalculator.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinancialCalculator.Web.Controllers;
+
+public class CreditController : Controller
+{
+    private readonly ICreditService _creditService;
+
+    public CreditController(ICreditService creditService)
+    {
+        this._creditService = creditService;
+    }
+    
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return this.View();
+    }
+    
+    
+    [HttpPost]
+    public IActionResult Calculate(CreditInputDto input)
+    {
+        CreditResultDto creditResultDto = this._creditService.CalculateCreditResult(input);
+    
+        CreditResult creditResult = new()
+        {
+            TotalPayments = Math.Round(creditResultDto.TotalPayments, 2),      
+            TotalInterest = Math.Round(creditResultDto.TotalInterest, 2),     
+            TotalPrincipal = Math.Round(creditResultDto.TotalPrincipal, 2),
+            TotalInitialFees = Math.Round(creditResultDto.TotalInitialFees, 2),
+            TotalFees = Math.Round(creditResultDto.TotalFees, 2),
+            AverageMonthlyPayment = Math.Round(creditResultDto.AverageMonthlyPayment, 2),
+            AmortizationSchedule = creditResultDto.AmortizationSchedule,
+            Apr = Math.Round(creditResultDto.Apr, 2)
+        };
+
+        return this.View("Result", creditResult);
+    }
+}
--- FinancialCalculator.Web/Controllers/HomeController.cs
+++ FinancialCalculator.Web/Controllers/HomeController.cs
@@ -8,62 +8,16 @@ namespace FinancialCalculator.Web.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ICreditService _creditService;
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ICreditService creditService, ILogger<HomeController> _logger)
-    {
-        this._creditService = creditService;
-        this._logger = _logger;
-    }
-
     [HttpGet]
     public IActionResult Index()
     {
         return this.View();
     }
     
-    [HttpPost]
-    public IActionResult Calculate(CreditInputDto input)
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
     {
-        try
-        {
-
-            if (!this.ModelState.IsValid)
-            {
-                var errors = ModelState.Values.SelectMany(v => v.Errors);
-
-                
-                return this.View("Error");
-            }
-        
-            CreditResultDto creditResultDto = this._creditService.CalculateCreditResult(input);
-        
-            CreditResult creditResult = new()
-            {
-                TotalPayments = Math.Round(creditResultDto.TotalPayments, 2),      
-                TotalInterest = Math.Round(creditResultDto.TotalInterest, 2),     
-                TotalPrincipal = Math.Round(creditResultDto.TotalPrincipal, 2),
-                TotalInitialFees = Math.Round(creditResultDto.TotalInitialFees, 2),
-                TotalFees = Math.Round(creditResultDto.TotalFees, 2),
-                AverageMonthlyPayment = Math.Round(creditResultDto.AverageMonthlyPayment, 2),
-                AmortizationSchedule = creditResultDto.AmortizationSchedule,
-                Apr = Math.Round(creditResultDto.Apr, 2)
-            };
-
-            return this.View("Result", creditResult);
-        }
-        catch (Exception e)
-        {
-            this._logger.LogError(e, "An error occured");
-            return this.RedirectToAction("Index", "Home");
-        }
-        
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
-
-    // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    // public IActionResult Error()
-    // {
-    //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    // }
 }
