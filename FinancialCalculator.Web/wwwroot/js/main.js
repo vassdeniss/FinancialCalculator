1cb32ed1e8078c574759a00b9a98c1382a293862
Denis Vasilev <vassdeniss@gmail.com>
2024-12-26 01:19:53 +0200

    refactor: separated js into individual files

---
+++ FinancialCalculator.Web/wwwroot/js/main.js
@@ -0,0 +1,32 @@
+﻿const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// JavaScript for Auto Carousel
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel img');
+const totalImages = images.length;
+
+function showNextImage() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % totalImages;
+    images[currentIndex].classList.add('active');
+}
+
+setInterval(showNextImage, 3000); // Change image every 3 seconds
+const toggles = document.querySelectorAll('.accordion-toggle');
+
+toggles.forEach((toggle) => {
+    toggle.addEventListener('click', function () {
+        this.classList.toggle('active');
+        const content = this.nextElementSibling;
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+        } else {
+            content.style.display = 'block';
+        }
+    });
+});
