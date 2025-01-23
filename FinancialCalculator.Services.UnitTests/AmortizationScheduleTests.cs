e46bda0b8591da1728e1f2a0acab847be78178cf
tatyana.pencheva.16@gmail.com <Ob1593572486//*//>
2025-01-23 11:42:48 +0200

    need a fix on APR method

--- FinancialCalculator.Services.UnitTests/AmortizationScheduleTests.cs
+++ FinancialCalculator.Services.UnitTests/AmortizationScheduleTests.cs
@@ -9,6 +9,7 @@ namespace FinancialCalculator.Services.UnitTests
     {
         [TestCase("1000", "120", "5", 12, "33.94")]
         [TestCase("1000", "120", "5", 36, "14.65")]
+        [TestCase("999999999", "0","1259.82", 960, "550282.9694")]
         public void CalculateApr_ForAnnuityPayments(string loanAmountStr, string totalInitialFeesStr, string annualInterestRateStr, int termInMonths, string expectedAprStr)
         {
             // Arrange
@@ -35,7 +36,7 @@ namespace FinancialCalculator.Services.UnitTests
             Assert.That(apr.BankersRounding(2), Is.EqualTo(expectedApr));
         }
 
-        [TestCase("1000", 12, 3, 3, "5", "10", "100", "105",
+        /*[TestCase("1000", 12, 3, 3, "5", "10", "100", "105",
                   PaymentType.Annuity, "10", FeeType.Currency, "20", FeeType.Currency, "30", FeeType.Currency, "10", FeeType.Currency,
                   "5", FeeType.Currency, "0", FeeType.Currency, "0", FeeType.Currency, "120",
                   "1250", "300", "700", "50", "128.4868")]
@@ -85,7 +86,7 @@ namespace FinancialCalculator.Services.UnitTests
             Assert.That(result.TotalPrincipal, Is.EqualTo(expectedTotalPrincipal));
             Assert.That(result.TotalFees, Is.EqualTo(expectedTotalFees));
             Assert.That(result.Apr.BankersRounding(4), Is.EqualTo(expectedApr));
-        }
+        }*/
 
     }
 }
